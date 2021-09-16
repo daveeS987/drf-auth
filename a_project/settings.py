049e0d40cee48cf0254a750af0b1ac74eeb0949b
Davee Sok <daveesok@gmail.com>
2021-09-16 17:25:06 -0500

    added authentication with jwt

--- a_project/settings.py
+++ a_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-aes!j4^3aq&rnzzf&=af6%nsiu&ur39u1m8$+3hwx%_%@*7e32
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1", "localhost"]
 
 
 # Application definition
--- movie/views.py
+++ movie/views.py
@@ -2,19 +2,14 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 from .serializers import MovieSerializer
 from .models import Movie
 from .permissions import IsOwnerOrReadOnly
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 
 class MovieList(ListCreateAPIView):
-    # permission_classes = (IsAuthenticatedOrReadOnly,)
-    # permission_classes = (IsOwnerOrReadOnly, IsAuthenticatedOrReadOnly)
-    # permission_classes = (IsAuthenticated,)
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
 
 class MovieDetail(RetrieveUpdateDestroyAPIView):
-    # permission_classes = (IsOwnerOrReadOnly, IsAuthenticatedOrReadOnly)
-    # permission_classes = (IsAuthenticated,)
+    permission_classes = (IsOwnerOrReadOnly,)
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
