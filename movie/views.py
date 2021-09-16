b8580936fb420727a2081b99513f21277262dc77
Davee Sok <daveesok@gmail.com>
2021-09-15 23:49:47 -0500

    updated

--- movie/views.py
+++ movie/views.py
@@ -2,17 +2,19 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 from .serializers import MovieSerializer
 from .models import Movie
 from .permissions import IsOwnerOrReadOnly
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 
 class MovieList(ListCreateAPIView):
     # permission_classes = (IsAuthenticatedOrReadOnly,)
-    permission_classes = (IsOwnerOrReadOnly,)
+    # permission_classes = (IsOwnerOrReadOnly, IsAuthenticatedOrReadOnly)
+    # permission_classes = (IsAuthenticated,)
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
 
 class MovieDetail(RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsOwnerOrReadOnly,)
+    # permission_classes = (IsOwnerOrReadOnly, IsAuthenticatedOrReadOnly)
+    # permission_classes = (IsAuthenticated,)
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
